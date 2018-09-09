0d813263b7e100fc56e47c9bc0f4b7c975e0d77c
shuxiaoyuan <1362521868@qq.com>
2018-09-09 23:09:31 +0800

    面试题 3 两个问题的答案

---
+++ CodingInterviewChinese2_Java/src/_03/DuplicationInArray.java
@@ -0,0 +1,84 @@
+package _03;
+
+import java.util.Arrays;
+
+/**
+ * 面试题3：数组中重复的数字
+ * n 个元素，都在 0~n-1 之间，找出其中一个重复数字
+ */
+public class DuplicationInArray {
+    
+    public static void main(String[] args) {
+        // 长度为 n 的数组里包含一个或多个重复的数字
+        int[] testCase1 = {2, 3, 1, 0, 2, 5, 3};
+        System.out.println("testCase1:" + getDuplication(testCase1));
+        
+        // 数组中不含重复的数字
+        int[] testCase2 = {2, 3, 1, 0, 5, 4};
+        System.out.println("testCase2:" + getDuplication(testCase2));
+        
+        // 空指针
+        int[] testCase3 = null;
+        System.out.println("testCase3:" + getDuplication(testCase3));
+        
+        // 长度为 n 的数组中包含 0~n-1 之外的数字
+        int[] testCase4 = {2, 3, 1, 0, 2, 6};
+        System.out.println("testCase4:" + getDuplication(testCase4));
+    }
+
+    private static int getDuplication(int[] numbers) {
+        if(numbers == null) {
+            return -1;
+        }
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] < 0 || numbers[i] > numbers.length-1) {
+                return -1;
+            }
+        }
+        
+        //return byHash(numbers);
+        //return bySort(numbers);
+        return bySwap(numbers);
+    }
+
+    // 哈希表方式，时间 O(n) 空间 O(n)，无需修改原数组
+    private static int byHash(int[] numbers) {
+        boolean[] tmp = new boolean[numbers.length]; 
+        for(int n : numbers) {
+            if(tmp[n]) {
+                return n;
+            }
+            else {
+                tmp[n] = true;
+            }
+        }
+        return -1;
+    }
+    
+    // 对原数组进行排序然后顺序查找，时间 O(nlogn) 空间 O(1)
+    private static int bySort(int[] numbers) {
+        Arrays.sort(numbers);
+        for(int i = 0; i < numbers.length - 1; i++) {
+            if(numbers[i] == numbers[i+1]) {
+                return numbers[i];
+            }
+        }
+        return -1;
+    }
+    
+    // 交换原数组中的元素，时间 O(n) 空间 O(1)
+    private static int bySwap(int[] numbers) {
+        for(int i = 0; i < numbers.length; i++) {
+            while(numbers[i] != i) {
+                if(numbers[i] == numbers[numbers[i]]) {
+                    return numbers[i];
+                }
+                int tmp = numbers[i];
+                numbers[i] = numbers[numbers[i]];
+                numbers[tmp] = tmp;
+            }
+        }
+        return -1;
+    }
+    
+}
---
+++ CodingInterviewChinese2_Java/src/_03/DuplicationInArrayNoEdit.java
@@ -0,0 +1,67 @@
+package _03;
+
+/**
+ * 面试题3：不修改数组找出重复的数字
+ * n+1 个元素，都在 1~n 之内，找出其中一个重复数字
+ * 时间 O(nlogn) 空间 O(1)
+ */
+public class DuplicationInArrayNoEdit {
+
+    public static void main(String[] args) {
+        // 长度为 n+1 的数组里包含一个或多个重复的数字
+        int[] testCase1 = {2, 3, 5, 4, 3, 2, 6, 7};
+        System.out.println("testCase1:" + getDuplication(testCase1));
+        
+        // 空指针
+        int[] testCase2 = null;
+        System.out.println("testCase2:" + getDuplication(testCase2));
+        
+        // 长度为 n+1 的数组中包含 1~n 之外的数字
+        int[] testCase3 = {2, 3, 5, 4, 3, 0, 6, 7};
+        System.out.println("testCase3:" + getDuplication(testCase3));
+    }
+
+    public static int getDuplication(int[] numbers) {
+        if(numbers == null) {
+            return -1;
+        }
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] < 1 || numbers[i] > numbers.length-1) {
+                return -1;
+            }
+        }
+        
+        int start = 1;
+        int end = numbers.length-1;
+        while(start <= end) {
+            int middle = start + (end - start) / 2;
+            int n = getCount(numbers, start, middle);
+            if(start == end) {
+                if(n > 1) {
+                    return start;
+                }
+                else {
+                    break;
+                }
+            }
+            if(n > middle-start+1) {
+                end = middle;
+            }
+            else {
+                start = middle + 1;
+            }
+        }
+        return -1;
+    }
+    
+    private static int getCount(int[] numbers, int start, int end) {
+        int n = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] >= start && numbers[i] <= end) {
+                n++;
+            }
+        }
+        return n;
+    }
+    
+}
