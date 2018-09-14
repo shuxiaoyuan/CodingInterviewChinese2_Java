ade15ead04599de5ff1e254d3f872e8c624f4988
shuxiaoyuan <1362521868@qq.com>
2018-09-14 09:19:22 +0800

    面试题9：用两个栈实现队列

---
+++ CodingInterviewChinese2_Java/src/_09/QueueWithTwoStacks.java
@@ -0,0 +1,42 @@
+package _09;
+
+import java.util.ArrayDeque;
+
+public class QueueWithTwoStacks {
+
+    public ArrayDeque<Integer> stack1 = new ArrayDeque<>();   // 入队的栈
+    public ArrayDeque<Integer> stack2 = new ArrayDeque<>();   // 出队的栈
+    
+    public static void main(String[] args) {
+        QueueWithTwoStacks queue = new QueueWithTwoStacks();
+
+        //queue.deleteHead();
+
+        queue.appendTail(1);
+        queue.appendTail(2);
+        queue.appendTail(3);
+        queue.appendTail(5);
+        queue.appendTail(8);
+        
+        System.out.println(queue.deleteHead());
+        System.out.println(queue.deleteHead());
+
+    }
+
+    public void appendTail(int val) {
+        this.stack1.push(val);
+    }
+    
+    public int deleteHead() {
+        if(this.stack2.isEmpty()) {
+            while(!this.stack1.isEmpty()) {
+                this.stack2.push(this.stack1.pop());
+            }
+        }
+        if(this.stack2.isEmpty()) {
+            throw new RuntimeException("队列为空！");
+        }
+        return this.stack2.pop();
+    }
+    
+}
