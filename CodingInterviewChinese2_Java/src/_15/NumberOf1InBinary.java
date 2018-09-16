943d799fd22c0a5d8b6df0bd7b8228834a18ba6c
shuxiaoyuan <1362521868@qq.com>
2018-09-16 23:23:21 +0800

    面试题15：二进制中1的个数

---
+++ CodingInterviewChinese2_Java/src/_15/NumberOf1InBinary.java
@@ -0,0 +1,35 @@
+package _15;
+
+public class NumberOf1InBinary {
+
+    public static void main(String[] args) {
+        System.out.println(numsOf1(-1));
+    }
+
+    public static int numsOf1(int n) {
+        //return byShift(n);
+        return byN_1(n);
+    }
+    
+    public static int byShift(int n) {
+        int count = 0;
+        int flag = 1;
+        for(int i = 0; i < 32; i++) {
+            if((n & flag) != 0) {
+                count++;
+            }
+            flag = flag << 1;
+        }
+        return count;
+    }
+    
+    public static int byN_1(int n) {
+        int count = 0;
+        while(n != 0) {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+    }
+    
+}
