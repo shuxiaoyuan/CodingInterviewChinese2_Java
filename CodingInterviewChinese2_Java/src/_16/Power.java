ef026efe87fd5f9a376d9c9929c6748be65b78f4
shuxiaoyuan <1362521868@qq.com>
2018-09-18 09:47:07 +0800

    面试题16：数值的整数次方

---
+++ CodingInterviewChinese2_Java/src/_16/Power.java
@@ -0,0 +1,62 @@
+package _16;
+
+public class Power {
+
+    public static boolean gInvalidInput = false;
+    
+    public static void main(String[] args) {
+        System.out.println(power(2, 3) + " " + gInvalidInput);
+        System.out.println(power(2, 0) + " " + gInvalidInput);
+        System.out.println(power(2, -3) + " " + gInvalidInput);
+        System.out.println(power(-2, 3) + " " + gInvalidInput);
+        System.out.println(power(-2, -3) + " " + gInvalidInput);
+        System.out.println(power(0, -1) + " " + gInvalidInput);
+        System.out.println(power(0, 0) + " " + gInvalidInput);
+        System.out.println(power(0, 1) + " " + gInvalidInput);
+    }
+    
+    public static double power(double base, int exponent) {
+        gInvalidInput = false;
+        if(equal(base, 0) && exponent <= 0) {
+            gInvalidInput = true;
+            return 0.0;
+        }
+        long absExponent = exponent >= 0 ? exponent : -exponent;
+        double result = powerCoreBetter(base, absExponent);
+        if(exponent < 0) {
+            result = 1.0 / result;
+        }
+        return result;
+    }
+    
+    public static boolean equal(double a, double b) {
+        if(a - b > -0.0000001 && a - b < 0.0000001) {
+            return true;
+        }
+        return false;
+    }
+    
+    public static double powerCore(double base, long absExponent) {
+        double result = 1.0;
+        for(int i = 0; i < absExponent; i++) {
+            result *= base;
+        }
+        return result;
+    }
+    
+    public static double powerCoreBetter(double base, long absExponent) {
+        if(absExponent == 0) {
+            return 1.0;
+        }
+        if(absExponent == 1) {
+            return base;
+        }
+        double result = powerCoreBetter(base, absExponent >> 1);
+        result *= result;
+        if((absExponent & 0x1) == 1) {
+            result *= base;
+        }
+        return result;
+    }
+    
+}
